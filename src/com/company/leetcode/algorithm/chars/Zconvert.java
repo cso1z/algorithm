f23cb59c466c674c70644b2c5a4be3331443c5f8
cso1z <2356191596@qq.com>
2021-01-20 00:03:56 +0800

    Z 字形变换

---
+++ src/com/company/leetcode/algorithm/chars/Zconvert.java
@@ -0,0 +1,29 @@
+package com.company.leetcode.algorithm.chars;
+
+import com.company.leetcode.interfac.AlgorithmInterface;
+import com.company.leetcode.utils.Log;
+
+/**
+ * 6. Z 字形变换
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ * 链接：https://leetcode-cn.com/problems/zigzag-conversion
+ * time:2020.01.20
+ */
+public class Zconvert implements AlgorithmInterface {
+    @Override
+    public void initData() {
+        Log.o("Z 字形变换" + getClass().getSimpleName());
+        String origin = "0123456789";
+        Log.oln("原始数据：" + origin);
+    }
+
+    public String convert(String s, int numRows) {
+
+        return null;
+    }
+}
